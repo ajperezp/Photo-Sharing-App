8f04da04a42ed5d29365fbb639e400dfab5fe6ff
Adrian Perez <ajperez@Adrians-MacBook.local>
2020-12-29 15:18:30 -0500

    first commit

---
+++ ig-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f6f9ef58474f19792af200a918382838'),
+    },
+  },
+});
---
+++ ig-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6a741f52-b90f-4a7d-8c42-765901462bc8'
+};
