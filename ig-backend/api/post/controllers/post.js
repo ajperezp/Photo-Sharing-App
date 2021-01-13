d57104e580b0e696ca2fc22e09598eee901c315a
Adrian Perez <ajperez@Adrians-MacBook.local>
2021-01-13 08:30:17 -0500

    Create Componen

--- ig-backend/api/post/controllers/post.js
+++ ig-backend/api/post/controllers/post.js
@@ -1,8 +1,30 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      if (!data || !data.description) {
+        ctx.throw(400, "Please write a description");
+      }
+      if (!files || !files.image || !files.length) {
+        ctx.throw(400, "Please upload an image");
+      }
+      entity = await strapi.services.post.create(
+        { ...data, likes: 0 },
+        { files }
+      );
+    } else {
+      ctx.throw(400, "Please use multipart/form-data");
+    }
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+};
--- ig-frontend/src/App.js
+++ ig-frontend/src/App.js
@@ -1,53 +1,22 @@
-import React, { useEffect, useState } from "react";
-import Post from "./components/Post";
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Create from "./pages/Create";
 import "./App.css";
 
-const mockPosts = [
-  {
-    likes: 18,
-    description: "First Photo",
-    image: {
-      url: "/uploads/matthew_landers_v_Ydqd4ck_SM_unsplash_f41120fb50.jpg",
-    },
-  },
-  {
-    likes: 35,
-    description: "Second Photo",
-    image: {
-      url: "/uploads/matthew_landers_v_Ydqd4ck_SM_unsplash_f41120fb50.jpg",
-    },
-  },
-  {
-    likes: 50,
-    description: "Third Photo",
-    image: {
-      url: "/uploads/matthew_landers_v_Ydqd4ck_SM_unsplash_f41120fb50.jpg",
-    },
-  },
-];
-
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const getPosts = async () => {
-      const response = await fetch("http://localhost:1337/posts");
-      const data = await response.json();
-      setPosts(data);
-    };
-    getPosts();
-  }, []);
-
   return (
     <div className="App">
-      {posts.map((post) => (
-        <Post
-          key={post.description}
-          likes={post.likes}
-          description={post.description}
-          url={post.image && post.image.url}
-        />
-      ))}
+      <h2>App</h2>
+
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/create" exact component={Create} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
